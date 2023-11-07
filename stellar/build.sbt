a9c78f257d4233bc9378d3b2cf471c843cd1b0ae
Renjie Liu <liurenjie2008@gmail.com>
2023-11-07 11:53:22 +0800

    feat: Setup logging

--- stellar/build.sbt
+++ stellar/build.sbt
@@ -13,6 +13,8 @@ lazy val rootProject = (project in file(".")).settings(
       "com.softwaremill.sttp.tapir" %% "tapir-swagger-ui-bundle" % tapirVersion,
       "com.softwaremill.sttp.tapir" %% "tapir-json-circe" % tapirVersion,
       "org.apache.iceberg" % "iceberg-core" % icebergVersion,
+      "ch.qos.logback" % "logback-classic" % "1.2.10",
+      "com.typesafe.scala-logging" %% "scala-logging" % "3.9.4",
       "com.softwaremill.sttp.tapir" %% "tapir-sttp-stub-server" % tapirVersion % Test,
       "org.scalatest" %% "scalatest" % "3.2.16" % Test,
       "com.softwaremill.sttp.client3" %% "upickle" % "3.8.15" % Test
--- stellar/src/main/scala/io/stellar/Main.scala
+++ stellar/src/main/scala/io/stellar/Main.scala
@@ -1,7 +1,8 @@
 package io.stellar
 
+import com.typesafe.scalalogging.Logger
 import io.stellar.catalog.Endpoints
-import sttp.tapir.server.netty.{NettyFutureServer, NettyFutureServerOptions}
+import sttp.tapir.server.netty.{NettyFutureServer, NettyFutureServerOptions, NettyOptions}
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
@@ -9,18 +10,34 @@ import scala.io.StdIn
 import ExecutionContext.Implicits.global
 
 object App {
+  private val logger = Logger[App]
   def main(args: Array[String]) = {
     println("Hello, world!")
+    logger.debug(s"First debug info!")
+
+    val nettyOptions = NettyOptions.default.port(8090)
+    val serverLogger = NettyFutureServerOptions.defaultServerLog
+      .copy(logWhenReceived = true)
+      .logWhenHandled(true)
+      .logAllDecodeFailures(true)
+      .logAllDecodeFailures(true)
+
+    val nettySeverOptions = NettyFutureServerOptions.customiseInterceptors
+      .serverLog(serverLogger)
+      .options
+      .nettyOptions(nettyOptions)
+
     val program = {
       for {
-        binding <- NettyFutureServer().port(8090).addEndpoints(Endpoints.serverEndpoints).start()
+        binding <- NettyFutureServer(nettySeverOptions)
+          .addEndpoints(Endpoints.serverEndpoints)
+          .start()
         _ <- Future {
           println(s"Go to http://localhost:${binding.port}/docs to open SwaggerUI. Press ENTER key to exit.")
           StdIn.readLine()
         }
         stop <- binding.stop()
-      }
-        yield stop
+      } yield stop
     }
 
     Await.result(program, Duration.Inf)
